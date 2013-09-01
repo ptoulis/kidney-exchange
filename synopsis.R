12c6742ba73be2635c7c6f5e837235e1f7b15c22
ptoulis <panos.toulis@gmail.com>
2013-09-01 17:56:52 -0400

    Synopsis has basic code showing how to use this project

--- synopsis.R
+++ synopsis.R
@@ -2,5 +2,5 @@
 # and how this is structured.
 source("mechanisms.R")
 rke.pool = rrke.pool(m=3, n=30, uniform.pra=T)
-kpd = kpd.create(rke.pool, strategy.str="ctt", verbose=T)
+kpd = kpd.create(rke.pool, strategy.str="ttt", verbose=T)
 Run.Mechanism(kpd, mech="rCM", include.3way=T, verbose=T)
